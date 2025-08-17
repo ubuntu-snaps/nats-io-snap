1b828f5c728cdb916669f9aa02c16e42648f7153
lvming <lvming@restartech.com>
2025-08-17 14:36:27 +0800

    feat: remove heartbeat

--- overlay/bin/heartbeat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export NATS_URL=nats://iotbox.net:4222
-BODY=$(hostnamectl --j=short)
-echo $BODY
-exec $SNAP/bin/nats pub iotbox.heartbeat.$(cat /etc/machine-id) --count=3153600 --sleep=10s "$BODY" > /dev/null
