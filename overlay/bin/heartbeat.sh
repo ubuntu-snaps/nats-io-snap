798e695743bbc16800527b7d47f3dff8598702bc
lvming <lvming@restartech.com>
2025-08-17 12:03:19 +0800

    chore: update heartbeat.sh

--- overlay/bin/heartbeat.sh
+++ overlay/bin/heartbeat.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export NATS_URL=nats://iotbox.net:4222
-exec $SNAP/bin/nats pub iotbox.heartbeat.$(cat /etc/machine-id) --count=3153600 --sleep=10s "$(hostnamectl --j=short)"
+BODY=$(hostnamectl --j=short)
+echo $BODY
+exec $SNAP/bin/nats pub iotbox.heartbeat.$(cat /etc/machine-id) --count=3153600 --sleep=10s "$BODY" > /dev/null
